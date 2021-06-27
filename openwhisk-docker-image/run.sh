01c405b6eb8ec9ebfc4a1549f782a28e0b5a605f
Michele Sciabarra <github@sciabarra.com>
2021-06-27 18:21:52 +0200

    articolo moka

--- openwhisk-docker-image/run.sh
+++ openwhisk-docker-image/run.sh
@@ -4,4 +4,4 @@ docker run -ti \
     --network bridge -p 3232:3232 -p 3233:3233  \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -e HOST_EXTERNAL=localhost \
-    sciabarracom/openwhisk-standalone:2020-07-01
+    sciabarracom/openwhisk-standalone:2020-07-01 "$@"
